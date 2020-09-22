4f7cda32443b686a7fefc2dbbf841559ffd16a89
Rodrigo <rodrigodot42@gmail.com>
2020-09-22 00:51:27 -0300

    refactor: update  interfaces

--- src/data/useCases/authentication/authentication.api.spec.ts
+++ src/data/useCases/authentication/authentication.api.spec.ts
@@ -1,17 +1,8 @@
 import { AuthenticationApi } from './authentication.api'
-import { HttpPostClient } from 'data/interfaces/http/http-post-client'
+import { HttpPostClientSpy } from '../../mocks/http/mock-http-post-client'
 
 describe('Authentication', () => {
   it('Should call HttpPostClient using the exact url', async () => {
-    class HttpPostClientSpy implements HttpPostClient {
-      url?: string
-
-      async post (url: string): Promise<void> {
-        this.url = url
-        return Promise.resolve()
-      }
-    }
-
     const url = 'any_url'
     const httpPostClientSpy = new HttpPostClientSpy()
     const wrapper = new AuthenticationApi(url, httpPostClientSpy)
