4ca35c6dae8def9ce21a3e5e99b93fe207bd915f
Rodrigo <rodrigodot42@gmail.com>
2020-09-22 00:59:40 -0300

    refactor: add create a createWrapper factory function

--- src/data/useCases/authentication/authentication.api.spec.ts
+++ src/data/useCases/authentication/authentication.api.spec.ts
@@ -1,11 +1,25 @@
 import { AuthenticationApi } from './authentication.api'
 import { HttpPostClientSpy } from '../../mocks/http/mock-http-post-client'
 
+type WrapperTypes = {
+  wrapper: AuthenticationApi
+  httpPostClientSpy: HttpPostClientSpy
+}
+
+const createWrapper = (url = 'any_url'): WrapperTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy()
+  const wrapper = new AuthenticationApi(url, httpPostClientSpy)
+
+  return {
+    wrapper,
+    httpPostClientSpy
+  }
+}
+
 describe('Authentication', () => {
   it('Should call HttpPostClient using the exact url', async () => {
     const url = 'any_url'
-    const httpPostClientSpy = new HttpPostClientSpy()
-    const wrapper = new AuthenticationApi(url, httpPostClientSpy)
+    const { wrapper, httpPostClientSpy } = createWrapper()
     await wrapper.auth() 
 
     expect(httpPostClientSpy.url).toBe(url)
